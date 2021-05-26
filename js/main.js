f7ae129507c960a3c3da9d16be36ec23c9d6ca9d
unknown <sjun8932@gmail.com>
2021-05-26 12:36:51 +0900

    현재 활성 씬 반영하기

--- js/main.js
+++ js/main.js
@@ -49,9 +49,20 @@
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight; // 스크롤 높이를 강제로 늘려주어 스크롤이 많이 되게 하는 부분
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`; // 늘려준 스크롤 높이를 html상에 높이로 지정해주는 부분
         }
+
+        yOffset = window.pageYOffset;
+        let totalScrollHeight = 0; //페이지가 특정 위치에서 새로고침(F5) 가 되었을 경우를 totalScrollHeight와 yOffset과의 관계설정으로 나타낸다.
+        for (let i = 0; i <sceneInfo.length; i++) {
+            totalScrollHeight += sceneInfo[i].scrollHeight;
+            if (totalScrollHeight >= yOffset) {
+                currentScene = i;
+                break;
+            }
+        }
+        document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
 
-    function scrollLoop() {
+    function scrollLoop() { 
         prevScrollHeight = 0;
         for (let i = 0; i < currentScene; i++) {
             prevScrollHeight += sceneInfo[i].scrollHeight;
@@ -59,22 +70,25 @@
 
         if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) { //활성화 씬 png 참조
             currentScene++;
+            //document.body.setAttribute('id', `show-scene-${currentScene}`);
         }
 
         if (yOffset < prevScrollHeight) {
-            if (currentScene === 0) return; // current씬이 -값이 되는 것을 방지
+            if (currentScene === 0) return; // current씬이 -값이 되는 것을 방지 (모바일 또는 ios 기기)
             currentScene--;
+            //document.body.setAttribute('id', `show-scene-${currentScene}`);
         }
 
-        console.log(currentScene);
+        document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
 
-    window.addEventListener('resize', setLayout);
     window.addEventListener('scroll', () => {
         yOffset = window.pageYOffset;
         scrollLoop();
     });
-
-    setLayout();
+    // window.addEventListener('DOMContentLoaded', setLayout);
+    window.addEventListener('load', setLayout); // DomContentload와 load의 차이 : load는 웹페이지에 있는 모든 리소스를 다운받고 나서야 작동 Domcontentload는 html 돔구조만 로드되면 실행됨. 그래서 후자가 좀더 빨리 실행됨
+    window.addEventListener('resize', setLayout);
+    setLayout(); // 이 함수는 --> 0. 레이아웃을 초기화 한다. 1. 각 sceneInfo 배열에 있는 각 scene의 스크롤 높이를 잡아주고 실제 html의 스크롤 높이에 세팅을 한다
 
 })();
