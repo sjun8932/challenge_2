3c860a6e1bc23553dd65f5993b8ef9772d34f56f
unknown <sjun8932@gmail.com>
2021-05-26 10:18:20 +0900

    스크롤 처리 기본 개념

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 (() => {
 
+    let yOffset = 0; // window.pageYOffset 대신 쓸 변수 (이렇게 하면 편하니까)
+
     const sceneInfo = [
         {
             //0
@@ -42,12 +44,20 @@
     function setLayout() {
         // 각 스크롤 섹션의 높이 세팅
         for (let i = 0; i < sceneInfo.length; i++) {
-            sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
-            sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+            sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight; // 스크롤 높이를 강제로 늘려주어 스크롤이 많이 되게 하는 부분
+            sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`; // 늘려준 스크롤 높이를 html상에 높이로 지정해주는 부분
         }
     }
 
+    function scrollLoop() {
+        
+    }
+
     window.addEventListener('resize', setLayout);
+    window.addEventListener('scroll', () => {
+        yOffset = window.pageYOffset;
+        scrollLoop();
+    });
 
     setLayout();
 
